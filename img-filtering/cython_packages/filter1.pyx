70e6b32141ed50ab6b9b9a6402114c01f354c233
Tim Rainer <tim.rainer04@gmail.com>
2022-10-21 11:51:31 +0200

    faster dim implementation

--- img-filtering/cython_packages/filter1.pyx
+++ img-filtering/cython_packages/filter1.pyx
@@ -1,11 +1,9 @@
-def times_cy(r,g,b, times):
-    tr = []
-    tg = []
-    tb = []
+
+def times_cy(double[:,:] r, double[:,:] g, double[:,:] b, times):
+    cdef double[:] tr, tg, tb
+
     for i in range(0, len(r)-1):
-        temp_r = []
-        temp_g = []
-        temp_b = []
+        cdef double[:] temp_r, temp_g, temp_b
         for j in range(0, len(r[i])-1):
             temp_r.append(r[i][j] * times)
             temp_g.append(g[i][j] * times)
