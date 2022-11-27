77683bfea5edd504eacd76b96ad612273d0ed75f
Tim Rainer <tim.rainer04@gmail.com>
2022-11-27 18:31:15 +0100

    add inline methods to router calls for visibility

--- python-only-stuff/express.py/src/main.py
+++ python-only-stuff/express.py/src/main.py
@@ -4,18 +4,26 @@ import logging
 ## do your route logic in here, this is the only place where routers will be handled.
 
 main_router = Router()
-def func(request):
-    request.send_response(200)
-    request.send_header("Content-type", "text/html")
-    request.end_headers()
-    request.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-    request.wfile.write(bytes("<p>Request: %s</p>" % request.path, "utf-8"))
-    request.wfile.write(bytes("<body>", "utf-8"))
-    request.wfile.write(bytes("<p>You got in here!!!!.</p>", "utf-8"))
-    request.wfile.write(bytes("</body></html>", "utf-8"))
+main_router.get("/", lambda request: [
+    request.send_response(200),
+    request.send_header("Content-type", "text/html"),
+    request.end_headers(),
+    request.wfile.write(bytes("<p>Request: %s</p>" % request.path, "utf-8")),
+    request.wfile.write(bytes("<body>", "utf-8")),
+    request.wfile.write(bytes("<p>You got in here!!!!.</p>", "utf-8")),
+    request.wfile.write(bytes("</body></html>", "utf-8")),
     logging.warn("Hello World")
+] )
 
-main_router.get("/", func)
-
+main_router.get("/test", lambda request: [
+    request.send_response(200),
+    request.send_header("Content-type", "text/html"),
+    request.end_headers(),
+    request.wfile.write(bytes("<p>Request: %s</p>" % request.path, "utf-8")),
+    request.wfile.write(bytes("<body>", "utf-8")),
+    request.wfile.write(bytes("<p>You got in here too!!!!.</p>", "utf-8")),
+    request.wfile.write(bytes("</body></html>", "utf-8")),
+    logging.warn("Hello World")
+] )
 def handleRequest(request):
     main_router.handleRequest(request)
